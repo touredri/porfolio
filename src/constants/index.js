ebbde83c69254a5e26deb706a60f2d092f0c5fa2
touredri <dcompte223@gmail.com>
2023-07-29 01:35:57 +0100

    setup

---
+++ src/constants/index.js
@@ -0,0 +1,264 @@
+import {
+  frontend,
+  backend,
+  ux,
+  prototyping,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  git,
+  figma,
+  docker,
+  postgresql,
+  rubyrails,
+  graphql,
+  komikult,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  space,
+  coverhunt,
+  dcc,
+  kelhel,
+  microverse,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Frontend Developer',
+    icon: frontend,
+  },
+  {
+    title: 'Backend Developer',
+    icon: backend,
+  },
+  {
+    title: 'UI/UX Design',
+    icon: ux,
+  },
+  {
+    title: 'Software Prototyping',
+    icon: prototyping,
+  },
+];
+
+const technologies = [
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'TypeScript',
+    icon: typescript,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Redux Toolkit',
+    icon: redux,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  {
+    name: 'Rails',
+    icon: rubyrails,
+  },
+  {
+    name: 'graphql',
+    icon: graphql,
+  },
+  {
+    name: 'postgresql',
+    icon: postgresql,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+  {
+    name: 'figma',
+    icon: figma,
+  },
+  {
+    name: 'docker',
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: 'Front-End Developer',
+    company_name: 'AREPEL',
+    icon: coverhunt,
+    iconBg: '#333333',
+    date: 'Aug 2022 - Jan 2023',
+  },
+  {
+    title: 'Mentor (Volunteer)',
+    company_name: 'Microverse',
+    icon: microverse,
+    iconBg: '#333333',
+    date: 'Apr 2023 - Now',
+  },
+  {
+    title: 'Junior Software Engineer',
+    company_name: 'Freelancer',
+    icon: kelhel,
+    iconBg: '#333333',
+    date: 'May 2023 - Now',
+  },
+];
+
+const projects = [
+  {
+    id: 'project-1',
+    name: 'tchi-gnouma',
+    description: 'A delivery service website for our company as a landing page',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'mongodb',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: komikult,
+    repo: 'https://github.com/touredri/tchi-gnouma',
+    demo: 'https://touredri.github.io/tchi-gnouma/',
+  },
+  {
+    id: 'project-2',
+    name: 'Fuit Web App',
+    description:
+      'A small web application in mobile view that provides information about fruit nutritional',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'restapi',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'scss',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: leaderboard,
+    repo: 'https://github.com/touredri/fruit',
+    demo: 'https://dev--superb-taiyaki-212be8.netlify.app/',
+  },
+  {
+    id: 'project-3',
+    name: 'Space Station',
+    description: 'This is a single-page space info app built with React redux where I was collaborator',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: math,
+    repo: 'https://github.com/mahabubx7/space-travellers-hub',
+    demo: 'https://space-traveller-hub.netlify.app/',
+  },
+  {
+    id: 'project-4',
+    name: 'Movie Show',
+    description: `A single-page application that allows users to movie or show's ratings and its details.`,
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: movie,
+    repo: 'https://github.com/touredri/capston2',
+    demo: 'https://touredri.github.io/capston2/dist/',
+  },
+  {
+    id: 'project-5',
+    name: 'Nyeusi Fest Site',
+    description:
+      'This is a demo event website for a BootCamp to learn JavaScript .',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: nyeusi,
+    repo: 'https://github.com/touredri/JS-Bootcamp',
+    demo: 'https://touredri.github.io/JS-Bootcamp',
+  },
+];
+
+export { services, technologies, experiences, projects };
