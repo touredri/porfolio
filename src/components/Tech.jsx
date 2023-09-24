42525957f7fd6e4e074f08411a2ab14157a4c959
touredri <dcompte223@gmail.com>
2023-09-24 12:25:32 +0100

    teck color change

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -10,7 +10,7 @@ const Tech = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubTextLight}>My skills</p>
-        <h2 className={styles.sectionHeadTextLight}>Technologies.</h2>
+        <h2 className={styles.sectionHeadText}>Technologies.</h2>
       </motion.div>
 
       <div className="flex flex-wrap justify-center gap-10 mt-14">
